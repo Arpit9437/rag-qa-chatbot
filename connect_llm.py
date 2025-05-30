27a54ed5a37243922093aa7636797fcf1e4fb7fe
Arpit9437 <arpitkaple13@gmail.com>
2025-05-30 11:20:26 +0530

    first commit

---
+++ connect_llm.py
@@ -0,0 +1,55 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+HF_TOKEN = os.environ.get("HF_TOKEN")
+HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
+
+def load_llm(huggingface_repo_id):
+    llm_endpoint = HuggingFaceEndpoint(
+        repo_id=huggingface_repo_id,
+        huggingfacehub_api_token=HF_TOKEN,
+        max_new_tokens=512,
+        temperature=0.5
+    )
+    llm = ChatHuggingFace(llm=llm_endpoint)
+    return llm
+
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+Don't provide anything outside the given context.
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    return prompt
+
+DB_FAISS_PATH = "vectorstore/db_faiss"
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=load_llm(HUGGINGFACE_REPO_ID),
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k': 3}),
+    return_source_documents=True,
+    chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+)
+
+user_query = "how to cure cancer?"
+response = qa_chain.invoke({'query': user_query})
+
+print("RESULT: ", response["result"])
+print("SOURCE DOCUMENTS: ", response["source_documents"])
